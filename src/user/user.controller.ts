2edf5cff8e948e8a7139524fcae80c4c9e42ba29
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-06-13 21:52:38 +0800

    Add user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, InternalServerErrorException, NotFoundException, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDTO } from './dto/user_register.dto';
+import { UserUpdateDTO } from './dto/user_update.dto';
+
+@Controller('user')
+export class UserController {
+
+  constructor (
+    private readonly userService:UserService,
+  ) {}
+
+  @Post('/register')
+  async register(@Body(ValidationPipe) userDTO:UserDTO) {
+    console.log('working');
+    
+    if (await this.userService.checkEmail(userDTO.email))
+      throw new InternalServerErrorException ('Email already exist')
+    if (await this.userService.checkUsername(userDTO.username))
+      throw new InternalServerErrorException ('Email already exist')
+    return this.userService.register(userDTO)
+  }
+  
+  @Patch(':id')
+  async updateUser(@Param('id', ParseIntPipe) id:number, @Body(ValidationPipe) userUpdateDTO:UserUpdateDTO) {
+    if (await this.userService.findOne(id)) 
+      return await this.userService.updateUser(id, userUpdateDTO)
+    throw new NotFoundException ("User not found")
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id:number) {
+    if (await this.userService.findOne(id))
+      return await this.userService.deleteUser(id)
+  }
+}
