f93c0b26db665900f27fbdd90255119c2a5d2b5a
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-06-13 21:50:59 +0800

    Add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { USER_TYPE } from '../app.constants';
+
+@Entity({ name: 'user' })
+export class UserModel {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  firstName: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  lastName: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  email: string;
+
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: USER_TYPE, nullable: false })
+  user_type?: USER_TYPE;
+}
