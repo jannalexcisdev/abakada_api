a9d310536d02195025d21182d95d30d396a08b05
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-06-13 21:52:49 +0800

    Add user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from '../models/user.model';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserModel,
+    ])
+  ],
+  controllers: [UserController],
+  providers: [UserService]
+})
+export class UserModule { }
